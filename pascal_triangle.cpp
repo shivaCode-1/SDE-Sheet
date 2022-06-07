4e86e75975ee882af1b073ebc28de75fe964823d
Shivang Sharma <107042281+shivaCode-1@users.noreply.github.com>
2022-06-07 17:01:45 +0530

    Create pascal_triangle.cpp

---
+++ pascal_triangle.cpp
@@ -0,0 +1,13 @@
+vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(int i = 0;i<numRows;i++)
+        {
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                temp[j] = res[i-1][j-1] + res[i-1][j]; //sum of previous and current column of previous row.
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
