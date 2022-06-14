97f5ba975e59d4030970c1a8173f109b0d92c459
Shivang Sharma <107042281+shivaCode-1@users.noreply.github.com>
2022-06-14 17:33:06 +0530

    Update topological_sort.cpp

--- graphs/topological_sort.cpp
+++ graphs/topological_sort.cpp
@@ -2,6 +2,7 @@
 //to perfrom this, run a normal dfs and after the dfs call is completed, insert the current node in a vector.
 //reverse the final vector.
 //this makes sure the all topological sorts are done before the final node is placed.
+//this also makes sure that the current node is placed in the vector only when all of its depth in inside the vector.
 void dfs(int i,vector<int> adj[],vector<int> &vis,vector<int> &res)
 	{
 	    if(vis[i]) return;
